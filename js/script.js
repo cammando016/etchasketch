6622421f68cf0439ffc8ff0a930c675c41ec7fc2
Cameron Anderson <cammando@hotmail.com.au>
2024-06-29 22:14:54 +1000

    Build etch-a-sketch with adjustable grid size based on user input

--- js/script.js
+++ js/script.js
@@ -1,6 +1,64 @@
 function createGrid () {
-    let i = 0
+    let i = 0;
+    const gridDiv = document.querySelector(".grid");
+
     while (i < 16) {
-        
+        let j = 0;
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+
+        while (j < 16) {
+            const gridCell = document.createElement("div");
+            gridCell.classList.add("grid-cell");
+            //gridCell.setAttribute("onmouseover", "changeBackground()")
+            gridRow.appendChild(gridCell);
+            j++;
+        }
+
+        gridDiv.appendChild(gridRow);
+        i++;
     }
 }
+
+function createUserGrid(squares) {
+    let i = 0;
+    const gridDiv = document.querySelector(".grid");
+    gridDiv.innerHTML = "";
+
+    while (i < squares) {
+        let j = 0;
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+
+        while (j < squares) {
+            const gridCell = document.createElement("div");
+            gridCell.classList.add("grid-cell");
+            //gridCell.setAttribute("onmouseover", "changeBackground()")
+            gridRow.appendChild(gridCell);
+            j++;
+        }
+
+        gridDiv.appendChild(gridRow);
+        i++;
+    }
+}
+
+createGrid();
+
+const gridButton = document.querySelector("#create-grid");
+gridButton.addEventListener("click", () => {createUserGrid(document.querySelector("#grid-slider").value)});
+
+// const gridDiv = document.querySelector(".grid");
+// gridDiv.addEventListener(
+//     "mouseover",
+//     (event) => {
+//         event.target.style.color = "grey";
+//     }
+// )
+
+const grid = document.querySelector(".grid");
+grid.addEventListener('mouseover', (event) => {
+    let target = event.target;
+    target.style.backgroundColor = "grey";
+    event.stopPropagation();
+})
